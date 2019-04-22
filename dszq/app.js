66701646c0d2da45fa54a950171a557b82541b21
1000copy <1000copy@gmail.com>
2019-04-22 13:09:15 +0900

    add search word

--- dszq/app.js
+++ dszq/app.js
@@ -1,5 +1,8 @@
 var express = require('express')
 var app = express()
+const bodyParser = require('body-parser');
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 var mongoose = require('mongoose')
 var dbf = mongoose.connect('mongodb://localhost/dszq',{ useNewUrlParser: true});
 var db = mongoose.connection;
@@ -8,10 +11,43 @@ app.get('/book',async (req,res,next)=>{
   // res.send('books')
   var book = require('./lib/book')
   var current_page =  req.query.page
+  var search =  req.query.search
+  var docs = await book.search(req,res,current_page,search)
+  var pages = await book.searchPages(req,res,search)
+  res.render('book.ejs',{
+    books:docs,current_page:current_page,pages:pages,base:'/book',
+    search:search})
+})
+app.post('/book',async (req,res,next)=>{
+  // res.send('books')
+  var book = require('./lib/book')
+  var current_page =  1
+  var docs = await book.search(req,res,1,req.body.search)
+  var pages = await book.searchPages(req,res,req.body.search)
+  console.log(pages," pages")
+  var search = req.body.search
+  res.render('book.ejs',
+    {books:docs,current_page:current_page,pages:pages,base:'/book',
+     search:search
+    })
+})
+app.get('/book/delete/:id',async (req,res,next)=>{
+  // res.send('books')
+  var book = require('./lib/book')
+  var id =  req.params.id
+  await book.delete(req,res,id)
   console.log(req.query.page)
-  var docs = await book.getList(req,res,current_page)
-  var pages = await book.getPages(req,res)
-  res.render('book.ejs',{books:docs,current_page:current_page,pages:pages,base:'/book'})
+  res.redirect('/book?page='+req.query.page)
+})
+app.get('/book/edit/:id',async (req,res,next)=>{
+  var book = require('./lib/book')
+  var book = await book.getBook(req,res,req.params.id)
+  res.render('bookedit.ejs',{book:book,current_page:req.query.page})
+})
+app.post('/book/edit/:id', async(req,res,next)=>{
+  var book = require('./lib/book')
+  await book.update(req,res,req.body.id,Object.assign({},req.body))
+  res.redirect('/book?page='+req.query.page)
 })
 app.listen(3000,()=>{
 	console.log('dszq starting...')
--- dszq/lib/book.js
+++ dszq/lib/book.js
@@ -2,7 +2,6 @@ var mongoose = require('mongoose');
 var BookModel = require('./model').BookModel
 var pageSize = 10
 exports.getList = async function(req,res,currentPage){
-    console.log('MongoDB Opened!');    
     var pageSize = 10
     if (!currentPage)
         currentPage = 1
@@ -10,5 +9,26 @@ exports.getList = async function(req,res,currentPage){
 }
 exports.getPages = async function(req,res){
     var docs =  await BookModel.find({})
-    return Math.round(docs.length / pageSize)
+    return Math.ceil(docs.length / pageSize)
+}
+exports.search = async function(req,res,currentPage,search){
+	var pageSize = 10
+    if (!currentPage)
+        currentPage = 1
+    var s = {$regex: search, $options: 'i'}
+    return await BookModel.find({title:s},{},{ skip: (currentPage - 1 )*pageSize, limit: pageSize })
+}
+exports.searchPages = async function(req,res,search){
+	var s = {$regex: search, $options: 'i'}
+    var docs =  await BookModel.find({title:s})
+    return Math.ceil(docs.length / pageSize)
+}
+exports.delete = async function(req,res,id){
+    return await BookModel.findOneAndDelete({_id:id})
+}
+exports.getBook = async function(req,res,id){
+    return await BookModel.findById(id)
+}
+exports.update = async function(req,res,id,obj){
+    return await BookModel.findOneAndReplace({_id:id},obj)
 }
