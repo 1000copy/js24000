172b1feee5127cab53e3d5cf3815ee39643a461f
1000copy <1000copy@gmail.com>
2019-05-22 16:40:49 +0800

    add borrow

--- dszq1/lib/model.js
+++ dszq1/lib/model.js
@@ -8,3 +8,9 @@ exports.BookModel = mongoose.model('BookSchema',new mongoose.Schema({
     title: {type: String},
     cover:{type:Buffer}
 }));
+exports.BorrowModel = mongoose.model('BorrowSchema',new mongoose.Schema({
+    comment: {type: String},
+    userid : {type:mongoose.Schema.Types.ObjectId},
+    bookid : {type:mongoose.Schema.Types.ObjectId},
+    date:{ type: Date, default: Date.now }
+}));
--- dszq1/router/borrow.js
+++ dszq1/router/borrow.js
@@ -10,5 +10,14 @@ router.get('/:id',async function(req,res){
 		console.log(e)
 	}
 })
+router.post('/:id',bodyparser.urlencoded({ extended: false }),async function(req,res){
+// router.post('/:id',async function(req,res){
+	try{
+		var coverid = req.params.id
+		res.send('borrow,'+coverid+req.body.comment)
+	}catch(e){
+		console.log(e)
+	}
+})
 
 module.exports = router
